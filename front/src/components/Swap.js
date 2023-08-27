648e38ce7b77417561fe241c652e56a53faf6b1b
류경표 <kyongpyo0626@gmail.com>
2023-08-27 16:03:25 +0900

    1inch swap api

--- front/src/components/Swap.js
+++ front/src/components/Swap.js
@@ -1,15 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { Input, Popover, Radio, Modal, message } from "antd";
-import {
-  ArrowDownOutlined,
-  DownOutlined,
-  SettingOutlined,
-} from "@ant-design/icons";
+import { Input, Modal } from "antd";
+import { ArrowDownOutlined, DownOutlined } from "@ant-design/icons";
 import tokenList from "../tokenList.json";
 import axios from "axios";
+import { useSendTransaction, useWaitForTransaction } from "wagmi";
 
-function Swap() {
-  const [slippage, setSlippage] = useState(2.5);
+function Swap(props) {
+  const { address, isConnected } = props;
   const [tokenOneAmount, setTokenOneAmount] = useState(null);
   const [tokenTwoAmount, setTokenTwoAmount] = useState(null);
   const [tokenOne, setTokenOne] = useState(tokenList[0]);
@@ -18,10 +15,22 @@ function Swap() {
   const [changeToken, setChangeToken] = useState(1);
   // backend에 요청 후 저장할 price
   const [prices, setPrices] = useState(null);
+  // transaction 세부 정보
+  const [txDetails, setTxDetails] = useState({
+    to: null,
+    data: null,
+    value: null,
+  });
 
-  function handleSlippageChange(e) {
-    setSlippage(e.target.value);
-  }
+  // transaction
+  const { data, sendTransaction } = useSendTransaction({
+    request: {
+      from: address,
+      to: String(txDetails.to),
+      data: String(txDetails.data),
+      value: String(txDetails.value),
+    },
+  });
 
   function changeAmount(e) {
     setTokenOneAmount(e.target.value);
@@ -71,8 +80,29 @@ function Swap() {
       params: { addressOne: one, addressTwo: two },
     });
 
-    console.log(res.data);
+    // console.log(res.data);
     setPrices(res.data);
+  }
+
+  // tx 보내고 있는지 확인
+  // 1inch Network로 지갑에 있는 토큰을 교환하도록 승인받기
+  // 1inch Approve API Swagger(https://portal.1inch.dev/documentation/swap/swagger)
+  async function fetchDexSwap() {
+    const allowance = await axios.get(
+      `https://api.1inch.dev/swap/v5.2/1/approve/allowance?tokenAddress=${tokenOne.address}&walletAddress=${address}`
+    );
+
+    // 허용이 되지 않았다면
+    if (allowance.data.allowance === "0") {
+      // tx Data get
+      const approve = await axios.get(
+        `https://api.1inch.dev/swap/v5.2/1/approve/transaction?tokenAddress=${tokenOne.address}`
+      );
+      setTxDetails(approve.data);
+      console.log("not approve");
+      return;
+    }
+    console.log("make swap");
   }
 
   // API 요청에 따른 결과 변경으로 useEffect로 tokenOne, tokenTwo 설정
@@ -80,18 +110,12 @@ function Swap() {
     fetchPrices(tokenList[0].address, tokenList[1].address);
   }, []);
 
-  const settings = (
-    <>
-      <div>Slippage Tolerance</div>
-      <div>
-        <Radio.Group value={slippage} onChange={handleSlippageChange}>
-          <Radio.Button value={0.5}>0.5%</Radio.Button>
-          <Radio.Button value={2.5}>2.5%</Radio.Button>
-          <Radio.Button value={5}>5%</Radio.Button>
-        </Radio.Group>
-      </div>
-    </>
-  );
+  useEffect(() => {
+    if (txDetails.to && isConnected) {
+      sendTransaction();
+    }
+  }, [txDetails]);
+
   return (
     <>
       <Modal
@@ -122,37 +146,42 @@ function Swap() {
       <div className="tradeBox">
         <div className="tradeBoxHeader">
           <h4>Swap</h4>
-          <Popover
-            content={settings}
-            title="Settings"
-            trigger="click"
-            placement="bottomRight"
-          >
-            <SettingOutlined className="cog" />
-          </Popover>
         </div>
         <div className="inputs">
-          <Input
-            placeholder="0"
-            value={tokenOneAmount}
-            onChange={changeAmount}
-          />
-          <Input placeholder="0" value={tokenTwoAmount} disabled={true} />
-          <div className="switchButton" onClick={switchTokens}>
-            <ArrowDownOutlined className="switchArrow" />
-          </div>
           <div className="assetOne" onClick={() => openModal(1)}>
             <img src={tokenOne.img} alt="assetOneLogo" className="assetLogo" />
             {tokenOne.ticker}
             <DownOutlined />
           </div>
+
+          <Input
+            placeholder="0"
+            value={tokenOneAmount}
+            onChange={changeAmount}
+            disabled={!prices}
+          />
+
+          <div className="switchButton" onClick={switchTokens}>
+            <ArrowDownOutlined className="switchArrow" />
+          </div>
+
           <div className="assetTwo" onClick={() => openModal(2)}>
             <img src={tokenTwo.img} alt="assetOneLogo" className="assetLogo" />
             {tokenTwo.ticker}
             <DownOutlined />
           </div>
+          <Input
+            placeholder="0"
+            value={tokenTwoAmount}
+            onChange={changeAmount}
+            disabled={true}
+          />
         </div>
-        <div className="swapButton" disabled={!tokenOneAmount}>
+        <div
+          className="swapButton"
+          disabled={!tokenOneAmount || !isConnected}
+          onClick={fetchDexSwap}
+        >
           Swap
         </div>
       </div>
